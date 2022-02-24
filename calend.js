7f486ed3f2f5c34d73a4084ba79b7ad7fe554149
javiercaliz <javicaliz00@gmail.com>
2022-02-24 12:32:30 -0300

    inicio de funcion vacaciones

--- calend.js
+++ calend.js
@@ -7,6 +7,19 @@ function pedirFecha() {
     return inicio;
 } 
 
+function vacaciones(){
+    let vacaciones = [];
+    let inicioVacaciones2021 = new Date('2021-12-23');
+    inicioVacaciones2021.setDate(inicioVacaciones2021.getDate()+1);
+    let finVacaciones2021 = new Date('2022-01-16');
+    finVacaciones2021.setDate(finVacaciones2021.getDate()+1);
+    
+
+
+
+
+
+}
 
 
 
